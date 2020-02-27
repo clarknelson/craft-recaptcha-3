b6274bb8877e374c4d869b98cae4cb48ec4a1dc7
Clark Nelson <mail@clarknelson.com>
2020-02-27 15:41:56 -0600

    modify anon access

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -46,7 +46,7 @@ class DefaultController extends Controller
      *         The actions must be in 'kebab-case'
      * @access protected
      */
-    protected $allowAnonymous = true;
+    protected $allowAnonymous = ['default'];
 
     // Public Methods
     // =========================================================================
